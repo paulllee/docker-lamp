8d854d29560fec05cae5c4508dceca1b0f5a630b
Paul Lee <paulllee.md@gmail.com>
2023-01-06 21:32:35 +0000

    Change sed search strings

--- bin/apache/installation/apache-configuration.sh
+++ bin/apache/installation/apache-configuration.sh
@@ -4,8 +4,8 @@
 # Company: Lyquix
 
 # editing the config files
-sed -i 's/Timeout 300/Timeout 60/' /etc/apache2/apache2.conf
-sed -i 's/MaxKeepAliveRequests 100/MaxKeepAliveRequests 0/' /etc/apache2/apache2.conf
+sed -i 's/Timeout.*/Timeout 60/' /etc/apache2/apache2.conf
+sed -i 's/MaxKeepAliveRequests.*/MaxKeepAliveRequests 0/' /etc/apache2/apache2.conf
 sed -i '/<Directory \/srv\/>/{
 n
 n
