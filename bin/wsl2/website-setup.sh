4c8224cb4215b6658f0c381ebfc95ca7bffe6a8e
Paul Lee <paulllee.md@gmail.com>
2023-01-06 20:03:01 +0000

    Add the rest of the code from original lamp setup

--- bin/wsl2/website-setup.sh
+++ bin/wsl2/website-setup.sh
@@ -19,6 +19,14 @@ do
     echo 'Enter the production website domain NAME (ex: lyquix):'
     read WEBSITE_DOMAIN_NAME
 
+	echo ''
+    echo 'Enter the development website subdomain NAME including the period if none, NA (ex: dev.):'
+    read WEBSITE_SUBDOMAIN_NAME
+
+    echo ''
+    echo 'Enter the production website domain EXTENSION including the period (ex: .com):'
+    read WEBSITE_DOMAIN_EXTENSION
+
     echo ''
     echo 'Which version of Ubuntu does this website use? 1804, 2004, or 2204 (match sensitive):'
     read UBUNTU_VERSION
@@ -33,6 +41,8 @@ do
 
     echo ''
     echo "WEBSITE DOMAIN NAME=$WEBSITE_DOMAIN_NAME"
+	echo "DEVELOPMENT WEBSITE SUBDOMAIN NAME=$WEBSITE_SUBDOMAIN_NAME"
+    echo "WEBSITE DOMAIN EXTENSION=$WEBSITE_DOMAIN_EXTENSION"
 	echo "UBUNTU VERSION=$UBUNTU_VERSION"
 	echo "HAS DATABASE=$HAS_DATABASE"
     echo 'Are the the credentials above correct? YES or NO (case sensitive):'
@@ -42,6 +52,8 @@ do
         USER_VERIFIED=NO
     fi
 done
+
+WEBSITE_ADDRESS=$WEBSITE_DOMAIN_NAME$WEBSITE_DOMAIN_EXTENSION
 
 # setting the mysql version. 1804 is the last to come with 5.7 as default
 if [ "$UBUNTU_VERSION" != "1804" ]
@@ -127,4 +139,118 @@ then
 	sed -i "s/database_name/$DATABASE_NAME/" $WORK_DIR/.env
 	sed -i "s/user_name/$DATABASE_USER/" $WORK_DIR/.env
 	sed -i "s/'database_password_is_kept_in_single_quotes'/'$DATABASE_PASS'/" $WORK_DIR/.env
+
+	# SSH for MySQL dump
+    echo ''
+    echo 'Open up PuTTY or Windows Terminal and SSH as root on the client side (check passwork for credentials)'
+    echo 'If you use Windows Terminal, here is how to SSH in:'
+    echo ''
+    echo "ssh root@$WEBSITE_ADDRESS"
+    echo 'Enter password: (check PassWork)'
+    echo "cd /srv/www/$WEBSITE_SUBDOMAIN_NAME$WEBSITE_ADDRESS/"
+    echo "mysqldump -u $DATABASE_USER -p $DATABASE_NAME > name.sql"
+    echo ''
+    echo 'In some circumstances, you will need to use the --no-tablespaces flag:'
+    echo "mysqldump -u $DATABASE_USER -p $DATABASE_NAME > name.sql --no-tablespaces"
+    echo ''
+    echo 'In some circumstances, you will need to be logged in as root user for MySQL:'
+    echo "mysqldump -u root -p $DATABASE_NAME > name.sql --no-tablespaces" 
+    echo ''
+    echo 'When you are done press Enter'
+    read USER_CHECKPOINT
+
+    # FTP using FileZilla to retrieve necessary files
+	echo ''
+    echo "FTP (using FileZilla) into the server as www-data user: download the sql file and put into the \\\\wsl\$\\Ubuntu\\home\\ubuntu\\docker-lamp\\$WEBSITE_DOMAIN_NAME\\data\\mysql-dump directory"
+    echo "You can use the .gitignore file and download all the files that are ignored to the \\\\wsl\$\\Ubuntu\\home\\ubuntu\\docker-lamp\\$WEBSITE_DOMAIN_NAME\\data\\www\\public_html directory"
+    echo ''
+    echo "Make sure to have at least the .htaccess, wp-config.php (WordPress), and configuration.php (Joomla)"
+    echo 'Look through the .htaccess and modify/remove the values to work for your local if needed (ex: check for redirecting and forcing ssl, password protection, ModPageSpeed must be turned Off)'
+    echo "You do NOT have to edit wp-config.php (WordPress) or configuration.php (Joomla), they will be automatically updated later in the script!"
+    echo ''
+    echo 'When you are done press Enter'
+    read USER_CHECKPOINT
+
+    echo ''
+    echo "Press Enter if you confirm that sql file is in the \\\\wsl\$\\Ubuntu\\home\\ubuntu\\docker-lamp\\$WEBSITE_DOMAIN_NAME\\data\\mysql-dump directory"
+    read USER_CHECKPOINT
+
+	# updating necessary values and srdb for WORDPRESS websites
+    cd $WORK_DIR/data/www/public_html
+
+    if [ "$CMS_TYPE" == 'WORDPRESS' ]
+    then
+		# docker container uses mysql as hostname
+		sed -i 's/localhost/mysql/' wp-config.php
+
+    elif [ "$CMS_TYPE" == 'JOOMLA' ]
+    then
+		# docker container uses mysql as hostname
+        sed -i "s/host = 'localhost'/host = 'mysql'/" configuration.php
+
+        # replacing all dev. addresses to test addresses
+        sed -i "s/$WEBSITE_SUBDOMAIN_NAME$WEBSITE_ADDRESS/localhost/g" configuration.php
+        # disabling ssl
+        sed -i "s/force_ssl = '1'/force_ssl = '0'/" configuration.php
+        sed -i "s/force_ssl = '2'/force_ssl = '0'/" configuration.php
+        # cookie domain to test address
+        sed -i "s/cookie_domain = '$WEBSITE_SUBDOMAIN_NAME$WEBSITE_ADDRESS'/cookie_domain = 'localhost'/" configuration.php
+    fi
+
+    JS_PATH="$(find $WORK_DIR/data/www/public_html -name 'js.php')"
+    
+    if grep -q "\$curl" "$JS_PATH"
+    then
+        echo ''
+        echo 'The Lyquix template has the js.php curl fix'
+    else
+        curl https://raw.githubusercontent.com/paulllee/docker-lamp/main/bin/wsl2/jsphp-fix -o jsphp-fix
+
+        sed -i '/ Remote script: /r jsphp-fix' $JS_PATH
+        sed -i '/curl_close($curl);/{
+            n
+            d
+            }' $JS_PATH
+
+        rm jsphp-fix
+
+        echo ''
+        echo 'The Lyquix template has been updated with the js.php curl fix'
+        echo 'Do not discard the changes of this file in SourceTree'
+        echo 'You have to keep the changes in the js.php file'
+    fi
+
+    CSS_PATH="$(find $WORK_DIR/data/www/public_html -name 'css.php')"
+    
+    if grep -q "\$curl" "$CSS_PATH"
+    then
+        echo ''
+        echo 'The Lyquix template has the css.php curl fix'
+    else
+        curl https://raw.githubusercontent.com/paulllee/docker-lamp/main/bin/wsl2/cssphp-fix -o cssphp-fix
+
+        sed -i '/ Remote stylesheet: /r cssphp-fix' $CSS_PATH
+        sed -i '/curl_close($curl);/{
+            n
+            d
+            }' $CSS_PATH
+
+        rm cssphp-fix
+
+        echo ''
+        echo 'The Lyquix template has been updated with the css.php curl fix'
+        echo 'Do not discard the changes of this file in SourceTree'
+        echo 'You have to keep the changes in the css.php file'
+    fi
+else
+    # FTP using FileZilla to retrieve necessary files for non-cms websites
+    echo "FTP (using FileZilla) into the server as www-data user: use the .gitignore file and download all the files that are ignored to the \\\\wsl\$\\Ubuntu\\home\\ubuntu\\docker-lamp\\$WEBSITE_DOMAIN_NAME\\data\\www\\public_html directory" 
+    echo 'Look through the .htaccess and modify/remove the values to work for your local if needed (ex: check for redirecting and forcing ssl, password protection, ModPageSpeed must be turned Off)'
+    echo ''
+    echo 'When you are done press Enter'
+    read USER_CHECKPOINT
 fi
+
+echo ''
+echo "The full website setup of $WEBSITE_DOMAIN_NAME is now complete!"
+echo "Go to the README for the final instructions."
